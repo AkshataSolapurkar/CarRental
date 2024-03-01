7fc6209815a52f2ab1cfda8d23da7518e9102432
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-03-01 18:44:32 +0530

    first commit

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div className='flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto'>
+        <div className=' flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, Book, or Rent a car - Quickly and easily! 
+            </h1>
+            <p className='hero__subtitle'>
+                Stremline your Car rental Experience with our effortless Booking process...
+            </p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default hero
