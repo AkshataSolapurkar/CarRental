64c2e58b2275ad79463eb1a30433cc74cd5b8392
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-03-04 23:56:21 +0530

    HeadlessUi2

--- src/app/components/SearchManufacture.tsx
+++ src/app/components/SearchManufacture.tsx
@@ -4,9 +4,19 @@ import { Fragment } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 import { Manufactureprops } from '../../../types'
 import Image from 'next/image'
+import { manufacturers } from '@/constants'
 
 const SearchManufacture = ({setmanufacture,manufacture}:Manufactureprops) => {
+    // This prop takes a function that determines how the selected value should be displayed in the input field.
+    // It receives the selected value as a string argument and should return the representation of that value that will be shown in the input field. In your case, 
+    //it seems to be just returning the value itself ((manufacture: string) => manufacture). 
+    //This means that the selected value will be displayed as is in the input field.
  const [Query,setQuery]=React.useState("")
+ const  filterManufactures = 
+    Query === "" ? manufacturers  
+    : manufacturers.filter((item)=>(
+      item.toLowerCase().replace(/\s+/g, " ").includes(Query.toLowerCase().replace(/\s+/g, " "))
+    ))
   return (
     <div className='search-manufacturer'>
       <Combobox>
@@ -19,11 +29,13 @@ const SearchManufacture = ({setmanufacture,manufacture}:Manufactureprops) => {
               alt='logo'
               className='ml-4'/>
             </Combobox.Button>
+
+
             <Combobox.Input
             placeholder='Volkswagen'
             className="search-manufacturer__input"
             displayValue= {(manufacture:string)=> manufacture}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)} // onchange in the inpute filed the targeted value will be stored in the SetQuery 
             />
 
             <Transition
