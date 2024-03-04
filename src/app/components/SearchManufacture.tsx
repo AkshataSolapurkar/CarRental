521e5cfc96a65aeccad716ecaa6d7024ebeec137
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-03-04 23:07:52 +0530

    HeadlessUi1

--- src/app/components/SearchManufacture.tsx
+++ src/app/components/SearchManufacture.tsx
@@ -1,11 +1,41 @@
+"use client"
 import React from 'react'
+import { Fragment } from 'react'
+import { Combobox, Transition } from '@headlessui/react'
 import { Manufactureprops } from '../../../types'
+import Image from 'next/image'
 
 const SearchManufacture = ({setmanufacture,manufacture}:Manufactureprops) => {
-
+ const [Query,setQuery]=React.useState("")
   return (
-    <div>
-      SearchManufacture
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'>
+            <Combobox.Button className="absolute top-[14px]">
+              <Image
+              src='/car-logo.svg'
+              width={20}
+              height={20}
+              alt='logo'
+              className='ml-4'/>
+            </Combobox.Button>
+            <Combobox.Input
+            placeholder='Volkswagen'
+            className="search-manufacturer__input"
+            displayValue= {(manufacture:string)=> manufacture}
+            onChange={(e) => setQuery(e.target.value)}
+            />
+
+            <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opactiy-100'
+            leaveTo='opacity-0'
+            afterLeave={()=> setQuery('')}>
+
+            </Transition>
+        </div>
+      </Combobox>
     </div>
   )
 }
